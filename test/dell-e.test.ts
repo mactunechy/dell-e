97761b34534331e6bb4b2dc68cef76f37da8a3fd
Dellan <mactunechy@gmail.com>
2023-01-29 19:33:28 +0400

    Initial commit

---
+++ test/dell-e.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DellE from '../lib/dell-e-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dell-e-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DellE.DellEStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
