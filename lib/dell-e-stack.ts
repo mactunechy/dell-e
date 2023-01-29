c64ec94101e79d09f2cd72ab562ccf6dbac9a721
Dellan <mactunechy@gmail.com>
2023-01-29 23:32:35 +0400

    add cdk deploy stage

--- lib/dell-e-stack.ts
+++ lib/dell-e-stack.ts
@@ -1,15 +1,11 @@
-import {
-  Stack,
-  StackProps,
-}  from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { S3BucketStack } from './s3-bucket-stack';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { S3BucketStack } from "./s3-bucket-stack";
 
 export class DellEStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const bucketsStack =  new S3BucketStack(this, 'S3BucketStack')
-
+    const bucketsStack = new S3BucketStack(this, "S3BucketStack");
   }
 }
--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -4,29 +4,50 @@ import {
   SecretValue,
   Stack,
   StackProps,
+  aws_codebuild as codebuild,
 } from "aws-cdk-lib";
 import { Construct } from "constructs";
+import { DellEStack } from "./dell-e-stack";
 
 export class PipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const pipeline = new codepipeline.Pipeline(this, "DellEPipeline", {
-      pipelineName: "DellEPipeline",
-    });
-
+    // Retrieve the source code from GitHub
     const sourceOutput = new codepipeline.Artifact();
     const sourceAction = new codepipeline_actions.GitHubSourceAction({
       actionName: "GitHub_Source",
       owner: "mactunechy",
-      repo: "aws-cdk",
+      repo: "dell-e",
       oauthToken: SecretValue.secretsManager("dell-e/github-token"),
       output: sourceOutput,
       branch: "main", // default: 'master'
     });
-    pipeline.addStage({
-      stageName: "Source",
-      actions: [sourceAction],
+
+    // Build the source code
+    const project = new codebuild.PipelineProject(this, "MyProject");
+
+    const buildAction = new codepipeline_actions.CodeBuildAction({
+      actionName: "CodeBuild",
+      project,
+      input: sourceOutput,
+      outputs: [new codepipeline.Artifact()], // optional
+      executeBatchBuild: true, // optional, defaults to false
+      combineBatchBuildArtifacts: true, // optional, defaults to false
+    });
+
+    new codepipeline.Pipeline(this, "DellEPipeline", {
+      pipelineName: "DellEPipeline",
+      stages: [
+        {
+          stageName: "Source",
+          actions: [sourceAction],
+        },
+        {
+          stageName: "Deploy",
+          actions: [buildAction],
+        },
+      ],
     });
   }
 }
